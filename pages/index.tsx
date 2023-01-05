ee8db42806d6c652248ad55cc8c11fe5e687ecfc
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2023-01-05 04:54:16 +0100

    removed interval for testing purposes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,19 @@ import { fetchAllMatches } from '../axios';
 
 export default function Home() {
   const updateBookiesMatches = useBookiesStore(state => state.updateBookiesMatches);
-  let interval = 10 * 60000; // x * 1 min 
+  // let interval = 10 * 60000; // x * 1 min 
     
   useEffect(() => {
-    const intervalRef = setInterval(() => {
+    // const intervalRef = setInterval(() => {
       fetchAllMatches(updateBookiesMatches);
-    }, interval);
+    // }, interval);
 
   
-    return () => {
-      clearInterval(intervalRef);
-    }
-  }, [interval, updateBookiesMatches]);
+    // return () => {
+    //   clearInterval(intervalRef);
+    //   alert("unmounted");
+    // }
+  }, [ updateBookiesMatches]);
   
 
   return (
